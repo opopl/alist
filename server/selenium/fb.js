5ced86dc88e532a9f55d841898bbfca6a554a855
me <me@localhost>
2022-12-23 22:15:14 +0200

    u

--- server/selenium/fb.js
+++ server/selenium/fb.js
@@ -7,13 +7,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send("Welocme to GeeksforGeeks!");
-});
-
 const path = require('path')
 const fs = require('fs')
 const util = require('util')
+
+const cons = require('consolidate');
+const nunjucks = require('nunjucks');
 
 const readFile = util.promisify(fs.readFile)
 
@@ -22,50 +21,73 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 const user = process.env.FB_LOGIN
 const pass = process.env.FB_PASS
 
-var webdriver = require('selenium-webdriver');
-var firefox   = require('selenium-webdriver/firefox');
+const firefox = require("selenium-webdriver/firefox");
 
-//var profile   = new firefox.Profile();
-//profile.setAcceptUntrustedCerts(true);
-//profile.setAssumeUntrustedCertIssuer(false); // `true` didn't work too
-//var browser   = new firefox.Driver(new firefox.Options().setProfile(profile));
+let driver, cookies = {}
 
-async function run() {
-  let driver = await new Builder()
-      .forBrowser(Browser.FIREFOX)
-      //.setPreference({ 'geo.enabled' : false })
-      //.setFirefoxOptions({ geoEnabled : false })
-      .build()
+const options = new firefox.Options()
+  .setPreference('geo.enabled', false)
+  .setPreference('geo.provider.network.url', 'data:application/json,{ "location" : { "latitude" : 43.7001100, "longitude" : -79.4163000, "accuracy" : 10 }}')
+  .setPreference('geo.provider.use_geoclue', false)
 
-  try {
+const viewsDir = path.resolve(__dirname, 'views')
+cons.requires.nunjucks = nunjucks.configure(viewsDir, {
+  autoescape: true,
+  express   : app
+});
+
+// view engine setup
+app.engine('html', cons.nunjucks);
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+  res.send("Welcome to GeeksforGeeks!");
+});
+
+app.get('/search', async (req, res) => {
+    await driver.get('http://www.duckduckgo.com');
+});
+
+app.get('/fb', async (req, res) => {
+    await driver.get('http://m.facebook.com');
+
+    for (let [name, value] of Object.entries(cookies)) {
+      await driver.manage().addCookie({ name, value })
+    }
+});
+
+app.get('/src', async (req, res) => {
+  const pageSource = await driver.wait(until.elementLocated(By.css('body')), 1000).getAttribute('innerHTML');
+  return res.send(pageSource)
+})
+
+async function initCookies() {
     const file = 'cookies.json'
     const jsonData = await readFile(file,'utf8')
     const data = JSON.parse(jsonData)
     const cookieJoin = data.cookie
     const cookieList = cookieJoin.split(';')
-
-    const cookies = {}
+    
     cookieList.forEach((x) => {
       const [ name, value ] = x.split('=')
       cookies[name] = value
     })
-    console.log(cookies);
+}
 
-    //await driver.get('https://m.facebook.com');
-    //for (let [name, value] of Object.entries(cookies)) {
-      //driver.manage().addCookie({ name, value })
-    //}
+async function run() {
+  await initCookies()
 
+  driver = await new Builder()
+      .forBrowser(Browser.FIREFOX)
+      .setFirefoxOptions(options)
+      .build()
+
+  try {
     app.listen(5000)
 
-    //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    //
-    //const username = await driver.findElement(By.xpath("//input[@name='email']"));
-    //username.sendKeys(user);
-
-    //const cookies = await driver.manage().getCookies()
-    //while(1){}
+    await driver.get('http://localhost:5000');
+    await driver.switchTo().newWindow('tab');
   } finally {
     //await driver.quit();
   }
