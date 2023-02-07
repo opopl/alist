a40e8baf2787f02e7c787398f63004c2d5ee755a
me <me@localhost>
2023-02-07 16:55:42 +0200

    c_PrjClass: export zip sub - rwPdf => 1

--- server/controllers/c_PrjClass.js
+++ server/controllers/c_PrjClass.js
@@ -945,7 +945,7 @@ const c_PrjClass = class {
       console.log({ sec_auth })
 
       // pdftk stage, overwrite extracted pages
-      const rwPdf = _.get(req, 'body.rwPdf', 0)
+      const rwPdf = _.get(req, 'body.rwPdf', 1)
 
       let minPage = _.get(req, 'body.minPage', 1); minPage = parseInt(minPage)
       let maxPage = _.get(req, 'body.maxPage', 5); maxPage = parseInt(maxPage)
